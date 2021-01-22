c81f75c7849c24cecbd4b88bae18cdfb7f54154e
John <johnab4444@gmail.com>
2021-01-22 18:21:02 -0500

    reverted back to add employee

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,6 +1,47 @@
 const Employee = require('../models/employee')
+const jwt = require('jsonwebtoken')
 
-const employee_index = (req, res) => {
+//handle errors
+const handleErrors = (err) => {
+  console.log(err.message, err.code)
+  let errors = {
+    employee_number: '',
+    first_name: '',
+    last_name: '',
+    pwd: '',
+    phone_number: '',
+    email: '',
+    address: '',
+    wage: '',
+    skill_level: ''
+  }
+
+  //duplicate error code
+  if (err.code === 11000) {
+    errors.email = 'Email already exists'
+    return errors
+  }
+
+  //validation errors
+  if (err.message.includes('Employee validation failed')) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message
+    })
+  }
+
+  return errors
+}
+
+const maxAge = 3 * 24 * 60 * 60 //3 days in seconds
+
+const createToken = (id) => {
+  //TODO make better secret to sign token
+  return jwt.sign({ id }, 'tuneup secret', {
+    expiresIn: maxAge
+  })
+}
+
+const employee_get = (req, res) => {
     Employee.find().sort({ createdAt: -1 })
         .then((result) => {
             res.send(result)
@@ -10,21 +51,29 @@ const employee_index = (req, res) => {
         })
 }
 
-const employee_create_employee = (req, res) => {
-    //console.log(req.body)
-    //const employee = new Employee(req.body)
-    const employee = new Employee({employeeNum: 111, firstName: 'Joe', lastName: 'Blow', pwd: '1234', phoneNum: '514-555-6969', email: 'joeBlows@hotmale.com', address: '5 Avenue Road', wage: 10.50, skillLevel: 0})
+const employee_post = async (req, res) => {
+  const { employee_number,
+    first_name,
+    last_name,
+    pwd,
+    phone_number,
+    email,
+    address,
+    wage,
+    skill_level } = req.body
   
-    employee.save()
-      .then((result) => {
-        res.redirect('/employee')
-      })
-      .catch((err) => {
-        throw err
-      })
+    try {
+      const employee = await Employee.create({ employee_number, first_name, last_name, pwd, phone_number, email, address, wage, skill_level })
+      const token = createToken(employee._id)
+      res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
+      res.status(201).json({ employee: employee._id })
+    } catch (err) {
+      const errors = handleErrors(err)
+      res.status(400).json({ errors })
+    }
 }
 
 module.exports = {
-    employee_index,
-    employee_create_employee
+    employee_get,
+    employee_post
 }

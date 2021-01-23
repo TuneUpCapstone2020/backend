d213b85f6c838c8f8ee6d8b9e8f32cd9ef1299cb
John <johnab4444@gmail.com>
2021-01-23 16:39:13 -0500

    added update and delete functions

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -32,13 +32,144 @@ const handleErrors = (err) => {
 
 }
 
-const vehicle_get = async (req, res) => {
+//START: ENDPOINTS FOR GET REQUESTS (Retrieve)
+
+const vehicle_get_client = async (req, res) => {
   const token = getDecodedToken(req)
   const client = await Client.findById(token.id).exec()
   await getVehiclesFromIds(client.vehicles).then((listOfVehicles, err) => {
-    if (err) { console.error(err); }
-    res.status(200).json(listOfVehicles)
+    if (err) { 
+      res.status(400).json({
+        message: 'An error occured!',
+        error: err
+      }); 
+    }else{
+      res.status(200).json(listOfVehicles)
+    }
   })
+}
+
+const vehicle_get_by_licence = (req, res) => {
+  Vehicle.find({ 
+    license: req.body.license,
+    isDeleted: false
+  })
+  .then((result) => {
+    res.status(200).json(result)
+  })
+  .catch((err) => {
+    res.status(400).json({
+      message: 'An error occured!',
+      error: err
+    })
+    throw err
+  })
+}
+
+//END: ENDPOINTS FOR GET REQUESTS
+
+//START: ENDPOINTS FOR POST REQUESTS (Create)
+
+const vehicle_post = async (req, res) => {
+  //TODO: make sure vehicle does not already exist (verify with vin)
+  const { make, model, nickname, license, year, mileage, vin_number } = req.body
+  const decodedId = getDecodedToken(req)
+
+  try {
+    const vehicle = await Vehicle.create({ make, model, nickname, license, year, mileage, vin_number })
+    await Client.addVehicle(decodedId.id, vehicle)
+    res.status(201).json({ 
+      message: 'New vehicle created!', 
+      vehicle: vehicle._id 
+    })
+  } catch (err) {
+    const errors = handleErrors(err)
+    res.status(400).json({ errors })
+  }
+}
+
+//END: ENDPOINTS FOR POST REQUESTS
+
+//START: ENDPOINTS FOR PUT REQUESTS (Update)
+
+const vehicle_update = async (req, res) => {
+  try{
+    const vehicle = await Vehicle.findById(req.body._id)
+
+    vehicle.make = req.body.make ? req.body.make : vehicle.make
+    vehicle.model = req.body.model ? req.body.model : vehicle.model
+    vehicle.nickname = req.body.nickname ? req.body.nickname : vehicle.nickname
+    vehicle.license = req.body.license ? req.body.license : vehicle.license
+    vehicle.year = req.body.year ? req.body.year : vehicle.year
+    vehicle.mileage = req.body.mileage ? req.body.mileage : vehicle.mileage
+    vehicle.vin_number = req.body.vin_number ? req.body.vin_number : vehicle.vin_number
+
+    vehicle.save(function(err){
+      if(err){
+        res.status(400).json({
+          message: 'An error occured!',
+          error: err
+        })
+      }else{
+        res.status(200).json({
+          message: 'Vehicle updated!',
+          id: vehicle._id
+        })
+      }
+    })
+  }catch(err){
+    res.status(400).json({
+      message: 'An error occured',
+      error: err
+    })
+  }
+}
+
+//END: ENDPOINTS FOR PUT REQUESTS
+
+//START: ENDPOINTS FOR DELETE REQUESTS (Delete)
+
+const vehicle_delete = async (req, res) => {
+  try{
+    const vehicle = await Vehicle.findByIdAndUpdate(req.params._id, { isDeleted: true })
+    vehicle.save(function(err){
+      if(err){
+        res.status(400).json({
+          message: 'An error occured!',
+          error: err
+        })
+      }else{
+        res.status(200).json({
+          message: 'Vehicle Deleted!',
+          id: vehicle._id
+        })
+      }
+    })
+  }catch(err){
+    res.status(400).json({
+      message: 'An error occured!',
+      error: err
+    })
+  }
+}
+
+//END: ENDPOINTS FOR DELETE REQUESTS
+
+
+module.exports = {
+  vehicle_get_client,
+  vehicle_post,
+  vehicle_get_by_licence,
+  vehicle_update,
+  vehicle_delete
+}
+
+function getDecodedToken(req) { //todo: maybe pass req.headers instead?
+  let token = req.headers['x-access-token'] || req.headers['authorization'];
+  token = token.replace("Bearer ", "")
+  token = jwt.decode(token, 'tuneup secret')
+  return token
+
 }
 
 const getVehiclesFromIds = async (listOfIds) => {
@@ -53,32 +184,3 @@ const getVehiclesFromIds = async (listOfIds) => {
     console.log(err)
   }
 };
-
-const vehicle_post = async (req, res) => {
-  //TODO: make sure vehicle does not already exist (verify with vin)
-  const { make, model, nickname, license, year, mileage, vin_number } = req.body
-  const decodedId = getDecodedToken(req)
-
-  try {
-    const vehicle = await Vehicle.create({ make, model, nickname, license, year, mileage, vin_number })
-    await Client.addVehicle(decodedId.id, vehicle)
-    res.status(201).json({ vehicle: vehicle._id })
-
-  } catch (err) {
-    const errors = handleErrors(err)
-    res.status(400).json({ errors })
-  }
-}
-
-module.exports = {
-  vehicle_get,
-  vehicle_post
-}
-
-function getDecodedToken(req) { //todo: maybe pass req.headers instead?
-  let token = req.headers['x-access-token'] || req.headers['authorization'];
-  token = token.replace("Bearer ", "")
-  token = jwt.decode(token, 'tuneup secret')
-  return token
-
-}

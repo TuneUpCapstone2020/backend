b5f4646f39e7b5ef0cbef72bf4e01cc0ec9f10fe
Brad Walsh <bradley.walsh.97@gmail.com>
2021-01-24 15:51:31 -0500

    fixed soft delete bug when creating 'new' vehicle

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -74,20 +74,54 @@ const vehicle_get_by_licence = (req, res) => {
 
 const vehicle_post = async (req, res) => {
   //TODO: make sure vehicle does not already exist (verify with vin)
-  const { make, model, nickname, license, year, mileage, vin_number } = req.body
   const decodedId = getDecodedToken(req)
 
   try {
-    const vehicle = await Vehicle.create({ make, model, nickname, license, year, mileage, vin_number })
-    await Client.addVehicle(decodedId.id, vehicle)
-    res.status(201).json({
-      message: 'New vehicle created!',
-      vehicle: vehicle._id
+    const newVehicle = await Vehicle.findOne({
+      license: req.body.license,
+      isDeleted: true
     })
+    if (newVehicle) {
+      const vehicle = await Vehicle.findOneAndUpdate(
+        newVehicle.license,
+        { isDeleted: false },
+        { new: true }
+      )
+      vehicle.make = req.body.make ? req.body.make : vehicle.make
+      vehicle.model = req.body.model ? req.body.model : vehicle.model
+      vehicle.nickname = req.body.nickname ? req.body.nickname : vehicle.nickname
+      vehicle.license = req.body.license ? req.body.license : vehicle.license
+      vehicle.year = req.body.year ? req.body.year : vehicle.year
+      vehicle.mileage = req.body.mileage ? req.body.mileage : vehicle.mileage
+      vehicle.vin_number = req.body.vin_number ? req.body.vin_number : vehicle.vin_number
+
+      vehicle.save()
+        .then((result) => {
+          console.log(`Deleted vehicle has been remade with the following info: ${result}`)
+          res.status(201).json({
+            message: 'New vehicle created!',
+            vehicle: result._id
+          })
+        })
+        .catch((err) => {
+          res.status(400).json({
+            message: 'An error occured!',
+            error: err
+          })
+        })
+    } else { //actually creating a new vehicle
+      const vehicle = await Vehicle.create(req.body)
+      await Client.addVehicle(decodedId.id, vehicle)
+      res.status(201).json({
+        message: 'New vehicle created!',
+        vehicle: vehicle._id
+      })
+    }
   } catch (err) {
     const errors = handleErrors(err)
     res.status(400).json({ errors })
   }
+
 }
 
 //END: ENDPOINTS FOR POST REQUESTS
@@ -108,6 +142,7 @@ const vehicle_update = async (req, res) => {
 
     vehicle.save()
       .then((result) => {
+        console.log(`Vehicle Updated: ${vehicle._id}`)
         res.status(200).json({
           message: 'Vehicle Updated!',
           vehicle: result._id
@@ -137,6 +172,7 @@ const vehicle_delete = async (req, res) => {
     const vehicle = await Vehicle.findByIdAndUpdate(req.query._id, { isDeleted: true })
     vehicle.save()
       .then((result) => {
+        console.log(`Vehicle Deleted: ${vehicle._id}`)
         res.status(200).json({
           message: 'Vehicle Deleted!',
           vehicle: result._id

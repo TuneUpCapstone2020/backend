4a66c251e4c2f70f0ea9207c5d7bba917dc78532
Brad Walsh <bradley.walsh.97@gmail.com>
2021-01-23 21:51:53 -0500

    Added essential fields and added time stamps

--- models/catalogProduct.js
+++ models/catalogProduct.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const CatalogService = require('./catalogService')
 
 const catalogProduct = new Schema({
     name: {
         type: String,
         required: true,
-        index: true
+        index: true //todo: unique?
     },
     garage_product_number: {
         type: String,
-        index: true
+        index: true,
+        unique: true
     },
     cost_price: {
         type: Number, //price stored in cents
@@ -21,20 +23,25 @@ const catalogProduct = new Schema({
     },
     description: {
         type: String,
+        //default: undefined
     },
     service: {
-        type: Schema.Types.ObjectId, //ref to the service
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'CatalogService', //ref to the service
+        default: undefined
     },
     sku: {
         type: String,
         required: true,
-        index: true
+        index: true,
+        unique: true
     },
     isDeleted: {
         type: Boolean,
         default: false
     }
-})
+}, { timestamps: true })
+
 
 const CatalogProduct = mongoose.model('CatalogProduct', catalogProduct)
 module.exports = CatalogProduct

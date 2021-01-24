d75cd57520e7542106c34b4d37a0c0f86b0003a1
Brad Walsh <bradley.walsh.97@gmail.com>
2021-01-23 21:56:13 -0500

    removed redundant throw err

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -17,8 +17,8 @@ const handleErrors = (err) => {
 
   //duplicate error code
   //if (err.code === 11000) {
-    //errors.vin_number = 'VIN already exists'
-    //return errors
+  //errors.vin_number = 'VIN already exists'
+  //return errors
   //}
 
   //validation errors
@@ -38,32 +38,31 @@ const vehicle_get_client = async (req, res) => {
   const token = getDecodedToken(req)
   const client = await Client.findById(token.id).exec()
   await getVehiclesFromIds(client.vehicles).then((listOfVehicles, err) => {
-    if (err) { 
+    if (err) {
       res.status(400).json({
         message: 'An error occured!',
         error: err
-      }); 
-    }else{
+      });
+    } else {
       res.status(200).json(listOfVehicles)
     }
   })
 }
 
 const vehicle_get_by_licence = (req, res) => {
-  Vehicle.find({ 
+  Vehicle.find({
     license: req.body.license,
     isDeleted: false
   })
-  .then((result) => {
-    res.status(200).json(result)
-  })
-  .catch((err) => {
-    res.status(400).json({
-      message: 'An error occured!',
-      error: err
+    .then((result) => {
+      res.status(200).json(result)
     })
-    throw err
-  })
+    .catch((err) => {
+      res.status(400).json({
+        message: 'An error occured!',
+        error: err
+      })
+    })
 }
 
 //END: ENDPOINTS FOR GET REQUESTS
@@ -78,9 +77,9 @@ const vehicle_post = async (req, res) => {
   try {
     const vehicle = await Vehicle.create({ make, model, nickname, license, year, mileage, vin_number })
     await Client.addVehicle(decodedId.id, vehicle)
-    res.status(201).json({ 
-      message: 'New vehicle created!', 
-      vehicle: vehicle._id 
+    res.status(201).json({
+      message: 'New vehicle created!',
+      vehicle: vehicle._id
     })
   } catch (err) {
     const errors = handleErrors(err)
@@ -93,7 +92,7 @@ const vehicle_post = async (req, res) => {
 //START: ENDPOINTS FOR PUT REQUESTS (Update)
 
 const vehicle_update = async (req, res) => {
-  try{
+  try {
     const vehicle = await Vehicle.findById(req.body._id)
 
     vehicle.make = req.body.make ? req.body.make : vehicle.make
@@ -104,20 +103,20 @@ const vehicle_update = async (req, res) => {
     vehicle.mileage = req.body.mileage ? req.body.mileage : vehicle.mileage
     vehicle.vin_number = req.body.vin_number ? req.body.vin_number : vehicle.vin_number
 
-    vehicle.save(function(err){
-      if(err){
+    vehicle.save(function (err) {
+      if (err) {
         res.status(400).json({
           message: 'An error occured!',
           error: err
         })
-      }else{
+      } else {
         res.status(200).json({
           message: 'Vehicle updated!',
           id: vehicle._id
         })
       }
     })
-  }catch(err){
+  } catch (err) {
     res.status(400).json({
       message: 'An error occured',
       error: err
@@ -130,22 +129,22 @@ const vehicle_update = async (req, res) => {
 //START: ENDPOINTS FOR DELETE REQUESTS (Delete)
 
 const vehicle_delete = async (req, res) => {
-  try{
+  try {
     const vehicle = await Vehicle.findByIdAndUpdate(req.params._id, { isDeleted: true })
-    vehicle.save(function(err){
-      if(err){
+    vehicle.save(function (err) {
+      if (err) {
         res.status(400).json({
           message: 'An error occured!',
           error: err
         })
-      }else{
+      } else {
         res.status(200).json({
           message: 'Vehicle Deleted!',
           id: vehicle._id
         })
       }
     })
-  }catch(err){
+  } catch (err) {
     res.status(400).json({
       message: 'An error occured!',
       error: err

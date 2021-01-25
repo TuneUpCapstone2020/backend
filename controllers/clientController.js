c3e05e4d860077896a6c47f50e995efa4d7a133d
John <johnab4444@gmail.com>
2021-01-24 22:59:43 -0500

    created all CRUD functions for client

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,5 +1,6 @@
 const Client = require('../models/client')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 //handle errors
 const handleErrors = (err) => {
@@ -57,7 +58,7 @@ const client_get_all = (req, res) => {
       res.status(200).json(result)
     })
     .catch((err) => {
-      console.log(`An error occured in: client_get_all`);
+      console.warn(`An error occured in: client_get_all`);
       res.status(400).json({
         message: 'An error occured!',
         error: err.message
@@ -75,7 +76,7 @@ const client_get_by_full_name = (req, res) => {
       res.status(200).json(result)
     })
     .catch((err) => {
-      console.log(`An error occured in: client_get_by_full_name`);
+      console.warn(`An error occured in: client_get_by_full_name`);
       res.status(400).json({
         message: 'An error occured!',
         error: err.message
@@ -93,7 +94,7 @@ const client_get_by_phone_number = (req, res) => {
       res.status(200).json(result)
     })
     .catch((err) => {
-      console.log(`An error occured in: client_get_by_phone_number`);
+      console.warn(`An error occured in: client_get_by_phone_number`);
       res.status(400).json({
         message: 'An error occured!',
         error: err.message
@@ -101,49 +102,147 @@ const client_get_by_phone_number = (req, res) => {
     })
 }
 
+const logout_get = (req, res) => {
+  console.log(`Logged out client`);
+  res.cookie('jwt', '', { maxAge: 1 })
+  res.status(200).json({ message: 'Token deleted ' })
+}
+
 //END: ENDPOINTS FOR GET REQUESTS
+
+//START: ENDPOINTS FOR POST REQUESTS (Create)
 
 const register_post = async (req, res) => {
   try {
-    const client = await Client.create(req.body)
-    console.log(`Created client ${client.email}`);
-    const token = createToken(client._id)
-    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
-    res.status(201).json({ 
-      message: 'New clent created!',
-      client: client._id
+    const newClient = await Client.findOne({
+      email: req.body.email,
+      deleted: true
     })
+    if (newClient) {
+      const client = await Client.findOneAndUpdate(
+        newClient.email,
+        { deleted: false },
+        { new: true }
+      )
+      client.first_name = req.body.first_name ? req.body.first_name : client.first_name
+      client.last_name = req.body.last_name ? req.body.last_name : client.last_name
+      client.address = req.body.address ? req.body.address : client.address
+      client.phone_number = req.body.phone_number ? req.body.phone_number : client.phone_number
+      client.email = req.body.email ? req.body.email : client.email
+      client.password = await hashPassword(client.password)
+
+      client.save()
+        .then((result) => {
+          console.log(`Delete client has been remade with the following info: ${result}`);
+          const token = createToken(result._id)
+          res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
+          res.status(201).json({
+            message: 'New client created!',
+            client: result._id
+          })
+        })
+        .catch((err) => {
+          res.status(400).json({
+            message: 'An error occured!',
+            error: err.message
+          })
+        })
+
+    } else {
+      req.body.password = await hashPassword(req.body.password)
+      const client = await Client.create(req.body)
+      console.log(`Created client ${client.email}`);
+      const token = createToken(client._id)
+      res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
+      res.status(201).json({
+        message: 'New clent created!',
+        client: client._id
+      })
+    }
   } catch (err) {
+    console.warn(`An error occured in register_post`);
     const errors = handleErrors(err)
     res.status(400).json({ errors })
   }
 }
 
 const login_post = async (req, res) => {
-  const { email, password } = req.body
-
   try {
-    const client = await Client.login(email, password)
+    const client = await Client.login(req.body.email, req.body.password)
+    console.log(`Logged in client ${client.email}`);
     const token = createToken(client._id)
     res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
     res.status(200).json({ client: client._id })
   }
   catch (err) {
+    console.warn(`An error occured in login_post`);
     const errors = handleErrors(err)
     res.status(400).json({ errors })
   }
 }
 
-const logout_get = (req, res) => {
-  res.cookie('jwt', '', { maxAge: 1 })
-  res.status(200).json({ msg: 'Token deleted ' })
-  //res.redirect('/')
+//END: ENDPOINTS FOR POST REQUESTS
+
+//START: ENDPOINTS FOR PUT REQUESTS (Update)
+
+const client_update = async (req, res) => {
+  try {
+    const token = getDecodedToken(req)
+    const client = await Client.findById(token.id)
+
+    client.first_name = req.body.first_name ? req.body.first_name : client.first_name
+    client.last_name = req.body.last_name ? req.body.last_name : client.last_name
+    client.address = req.body.address ? req.body.address : client.address
+    client.phone_number = req.body.phone_number ? req.body.phone_number : client.phone_number
+    client.email = req.body.email ? req.body.email : client.email
+  } catch (err) {
+    console.log(`An error occured in client_update`);
+    res.status(400).json({
+      message: 'An error occured!',
+      error: err.message
+    })
+  }
 }
 
+//END: ENDPOINTS FOR PUT REQUESTS
+
+//START: ENDPOINTS FOR DELETE REQUESTS (Delete)
+
+const client_delete = async (req, res) => {
+  try {
+    const token = getDecodedToken(req)
+    const client = await Client.findOneAndUpdate(token.id, { deleted: true })
+  } catch (err) {
+    console.log(`An error occured in client_delete!`);
+    res.status(400).json({
+      message: 'An error occured!',
+      error: err.message
+    })
+  }
+}
+
+//END: ENDPOINTS FOR DELETE REQUESTS
+
 module.exports = {
-  register_get,
-  login_get,
+  client_get_all,
+  client_get_by_full_name,
+  client_get_by_phone_number,
   register_post,
   login_post,
-  logout_get
+  logout_get,
+  client_update,
+  client_delete
+}
+
+function getDecodedToken(req) { //todo: maybe pass req.headers instead?
+  let token = req.headers['x-access-token'] || req.headers['authorization'];
+  token = token.replace("Bearer ", "")
+  token = jwt.decode(token, 'tuneup secret')
+  return token
+}
+
+const hashPassword = async (password) => {
+  //const salt = await bcrypt.genSalt() //TODO: add prints to see what the salt is and what the password is. 
+  //password = await bcrypt.hash(password, salt)
+  return password
 }

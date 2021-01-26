4fdd9eb6893007762d0fef1b558e4361beeb7e25
Brad Walsh <bradley.walsh.97@gmail.com>
2021-01-25 23:52:49 -0500

    updated field names and added indexing by date

--- models/appointment.js
+++ models/appointment.js
@@ -1,20 +1,26 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const CatalogService = require('./catalogService')
+const CatalogProduct = require('./catalogProduct')
+const Vehicle = require('./vehicle')
 
 const appointmentSchema = new Schema({
-    date: {
-        type: Date,
-        required: true
-    },
-    startTime: {
-        type: String,
-        required: true
-    },
     /*garageID: {
         type: Schema.Types.ObjectId,
         required: true
     },*/
-    skillLevel: {
+    date: {
+        type: Date,
+        required: true,
+        index: true
+    },
+    start_time: {
+        type: Date
+    },
+    end_time: {
+        type: String
+    },
+    skill_level: {
         type: Number,
         required: true
     },

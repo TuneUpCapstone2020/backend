8a9cb5fc7cfdb44b377b33b5bfcdde7b915231ad
Brad Walsh <bradley.walsh.97@gmail.com>
2021-01-30 02:27:50 -0500

    updated to make a dummy garage that works with current garage model

--- controllers/garageController.js
+++ controllers/garageController.js
@@ -1,30 +1,49 @@
 const Garage = require('../models/garage')
 
 const garage_index = (req, res) => {
-    Garage.find().sort({ createdAt: -1 })
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            throw err
-        })
+  Garage.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) => {
+      throw err
+    })
 }
 
 const garage_create_garage = (req, res) => {
-    //console.log(req.body)
-    //const garage = new Garage(req.body)
-    const garage = new Garage({name: 'Joe\'s Garage',owner: 'Joe Blow', email: 'joe@joesgarage.com', phoneNum: '514-555-6969', address: '420 Fixit Ave.'})
-  
-    garage.save()
-      .then((result) => {
-        res.redirect('/client')
+  //console.log(req.body)
+  //const garage = new Garage(req.body)
+  const garage = new Garage({
+    name: "Joe's Garage",
+    owner: 'Joe Blow',
+    email: 'joe@joesgarage.com',
+    phoneNum: '514-555-6969',
+    address: '420 Fixit Ave.',
+    standard_hourly_rate: '10000',
+    appointment_capacity: '15',
+    opening_time: '480',
+    closing_time: '960',
+  })
+
+  garage
+    .save()
+    .then((result) => {
+      //res.redirect('/client')
+      res.status(201).json({
+        message: 'New garage created!',
+        Garage: result._id,
       })
-      .catch((err) => {
-        throw err
+    })
+    .catch((err) => {
+      res.status(400).json({
+        message: 'Unable to create new garage',
+        error: err.message,
       })
+    })
 }
 
 module.exports = {
-    garage_index,
-    garage_create_garage
+  garage_index,
+  garage_create_garage,
 }

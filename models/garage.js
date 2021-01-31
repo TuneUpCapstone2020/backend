e7ae095921b09fa5b04fe259f206b3ea238765b5
Brad Walsh <bradley.walsh.97@gmail.com>
2021-01-31 18:51:51 -0500

    formatting

--- models/garage.js
+++ models/garage.js
@@ -2,62 +2,58 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const garageSchema = new Schema({
-
-    name: {
-        type: String,
-        required: true
-    },
-    owner: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    phoneNum: {
-        type: String,
-        required: true
-    },
-    address: {
-        type: String,
-        required: true
-    },
-    standard_hourly_rate: {
-        type: Number, //stored in cents
-        required: true
-    },
-    appointment_capacity: {
-        type: Number,
-        required: true
-    },
-    opening_time: {
-        type: Number, //store the time it opens in minutes since 12:00am
-        required: true
-    },
-    closing_time: {
-        type: Number, //time in minutes from 12:00am to closing
-        required: true
-    }
-
+  name: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phoneNum: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  standard_hourly_rate: {
+    type: Number, //stored in cents
+    required: true,
+  },
+  appointment_capacity: {
+    type: Number,
+    required: true,
+  },
+  opening_time: {
+    type: Number, //store the time it opens in minutes since 12:00am
+    required: true,
+  },
+  closing_time: {
+    type: Number, //time in minutes from 12:00am to closing
+    required: true,
+  },
 })
 
 garageSchema.statics.getAppointmentCapacity = function () {
-    return this.appointment_capacity
+  return this.appointment_capacity
 }
 
 garageSchema.statics.getStandardHourlyRate = function () {
-    return this.standard_hourly_rate
+  return this.standard_hourly_rate
 }
 
 garageSchema.statics.getOpeningTime = function () {
-    return this.opening_time
+  return this.opening_time
 }
 garageSchema.statics.getClosingTime = function () {
-    return this.closing_time
+  return this.closing_time
 }
-
-
 
 const Garage = mongoose.model('Garage', garageSchema)
 module.exports = Garage

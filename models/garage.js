ab9014860eff05d3c66efe197abadbbc4101bde0
John <johnab4444@gmail.com>
2021-01-30 19:39:12 -0500

    updated and added fields

--- models/garage.js
+++ models/garage.js
@@ -1,21 +1,28 @@
 const mongoose = require('mongoose')
+const Employee = require('./employee')
+const { isEmail } = require('validator')
 const Schema = mongoose.Schema
 
 const garageSchema = new Schema({
     
     name: {
         type: String,
-        required: true
+        required: true,
+        index: true
     },
     owner: {
         type: String,
-        required: true
+        required: true,
+        index: true
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, 'Please enter valid email']
     },
-    phoneNum: {
+    phone_number: {
         type: String,
         required: true
     },
@@ -23,8 +30,15 @@ const garageSchema = new Schema({
         type: String,
         required: true
     },
+    employees: [{
+        employee: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee' }
+    }],
+    deleted: {
+        type: Boolean,
+        default: false
+    }
     
-})
+}, { timestamps: true })
 
 const Garage = mongoose.model('Garage', garageSchema)
 module.exports = Garage

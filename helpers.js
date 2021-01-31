50c185d8935759a3e05bd837d2a7680339fffb72
Brad Walsh <bradley.walsh.97@gmail.com>
2021-01-31 13:45:48 -0500

    now uses dateformat for getTimeStamp

--- helpers.js
+++ helpers.js
@@ -1,16 +1,19 @@
-const getTimeStamp = () => {
-    let date_ob = new Date()
-    let date = ('0' + date_ob.getDate).slice(-2)
-    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2)
-    let year = date_ob.getFullYear()
-    let hours = date_ob.getHours()
-    let minutes = date_ob.getMinutes()
-    let seconds = date_ob.getSeconds()
+const dateFormat = require('dateformat')
 
-    return `${seconds}:${minutes}:${hours} ${date}-${month}-${year}`
+const getTimeStamp = () => {
+  let date_ob = new Date()
+  // let date = ('0' + date_ob.getDate).slice(-2)
+  // let month = ("0" + (date_ob.getMonth() + 1)).slice(-2)
+  // let year = date_ob.getFullYear()
+  // let hours = date_ob.getHours()
+  // let minutes = date_ob.getMinutes()
+  // let seconds = date_ob.getSeconds()
+
+  // return `${seconds}:${minutes}:${hours} ${date}-${month}-${year}`
+  //return dateFormat(date_ob, 'hh:MM:ss dd-mm-yyyy')
+  return dateFormat(date_ob, 'isoDateTime')
 }
 
-
 module.exports = {
-    getTimeStamp
+  getTimeStamp,
 }

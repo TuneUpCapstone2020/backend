dbc32b0f4e7ee2817f6060f991fa9b6f8cca82aa
Brad Walsh <bradley.walsh.97@gmail.com>
2021-02-03 17:03:12 -0500

    added missing route

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -15,6 +15,7 @@ router.get('/date/employee', appointmentController.appoints_get_by_date_and_empl
 router.get('/date/client', appointmentController.appoints_get_by_date_and_client)
 router.get('/date/range', appointmentController.appoints_get_by_date_range)
 router.get('/date/availability', appointmentController.appoints_get_availability_by_date)
+router.get('/date/availability/range', appointmentController.appoints_get_free_days_of_week)
 router.get('/archived', appointmentController.archived_appoints_get_all)
 router.get('/archived/user', appointmentController.archived_appoints_get_by_user)
 router.get('/archived/id', appointmentController.archived_appoints_get_by_id)

93cb28d81a8a14b640ac47f6b016482057eb4299
Brad Walsh <bradley.walsh.97@gmail.com>
2021-02-06 00:05:56 -0500

    added more endpoints

--- controllers/packageController.js
+++ controllers/packageController.js
@@ -1,5 +1,7 @@
 const helper = require('../helpers')
 const _ = require('lodash')
+const Garage = require('../models/garage')
+const CatalogService = require('../models/catalogService')
 
 const handleErrors = (err) => {
   console.warn(
@@ -42,6 +44,7 @@ const handleErrors = (err) => {
  *  published: boolean of if package is to be published immediately or not (true for yes, false for no)
 */
 const package_create = async (req, res) => {
+  //todo: make sure its not already in the db
   let newPackage = _.omitBy(req.body, _.isNil)
   newPackage.garageId = await Garage.findById(newPackage.garageId)
   const services = []
@@ -50,4 +53,181 @@ const package_create = async (req, res) => {
     services.push(await CatalogService.findById(serviceId))
   }
   newPackage.services = services
+  try {
+    const package = await Package.create(newPackage)
+    console.log(`New package created at ${helper.getTimeStamp()}`)
+    console.log(`New package id: ${package._id}`)
+    res.status(201).json({
+      message: `New package create!`,
+      id: package._id,
+    })
+  } catch (err) {
+    const errors = handleErrors(err)
+    console.warn(
+      `An error occured in package_create @ time: ${helpers.getTimeStamp()}`
+    )
+    console.log(`Error: ${err.message}`)
+    res.status(400).json({
+      message: `Unable to create package`,
+      error: errors,
+    })
+  }
+}
+
+//Retrieve
+const package_get_all = (req, res) => {
+  Package.find({ delete: false })
+    .sort({ createdAt: -1 })
+    .then((package) => {
+      console.log(`Get all packages @ time: ${helpers.getTimeStamp()}`)
+      res.status(200).json(package)
+    })
+    .catch((err) => {
+      console.warn(
+        `An error occured in: package_get_all @ time: ${helpers.getTimeStamp()}`
+      )
+      console.log(`Error: ${err.message}`)
+      res.status(400).json({
+        message: 'Unable to get all packages!',
+        error: err.message,
+      })
+    })
+}
+
+const package_get_all_published = (req, res) => {
+  Package.find({ delete: false, publish: true })
+    .sort({ createdAt: -1 })
+    .then((package) => {
+      console.log(
+        `Get all published packages @ time: ${helpers.getTimeStamp()}`
+      )
+      res.status(200).json(package)
+    })
+    .catch((err) => {
+      console.warn(
+        `An error occured in: package_get_all_published @ time: ${helpers.getTimeStamp()}`
+      )
+      console.log(`Error: ${err.message}`)
+      res.status(400).json({
+        message: 'Unable to get all published packages!',
+        error: err.message,
+      })
+    })
+}
+
+const package_get_all_unpublished = (req, res) => {
+  Package.find({ delete: false, publish: false })
+    .sort({ createdAt: -1 })
+    .then((package) => {
+      console.log(
+        `Get all unpublished packages @ time: ${helpers.getTimeStamp()}`
+      )
+      res.status(200).json(package)
+    })
+    .catch((err) => {
+      console.warn(
+        `An error occured in: package_get_all_published @ time: ${helpers.getTimeStamp()}`
+      )
+      console.log(`Error: ${err.message}`)
+      res.status(400).json({
+        message: 'Unable to get all unpublished packages!',
+        error: err.message,
+      })
+    })
+}
+
+/*
+ * Query params:
+ *  garageId: Id of garage we want to get all the packages for
+ */
+const package_get_by_garage = (req, res) => {
+  Package.find({ delete: false, garage: req.query.garageId })
+    .then((packages) => {
+      console.log(`Get packages by garage @ time: ${helpers.getTimeStamp()}`)
+      res.status(200).json(packages)
+    })
+    .catch((err) => {
+      console.warn(
+        `Error occured in package_get_by_garage @ time: ${helpers.getTimeStamp()}`
+      )
+      console.log(`Error: ${err.message}`)
+      res.status(400).json({
+        message: `Unable to get packages`,
+        error: err.message,
+      })
+    })
+}
+
+//In query params: name of package
+//todo: consider looking into fuzzy search/search index or create manually with compass
+const package_get_by_name = (req, res) => {
+  Package.find({ delete: false, name: req.params.name })
+    .then((packages) => {
+      console.log(`Get packages by name @ time: ${helpers.getTimeStamp()}`)
+      res.status(200).json({ packages })
+    })
+    .catch((err) => {
+      console.warn(
+        `Error occured in package_get_by_name @ time: ${helpers.getTimeStamp()}`
+      )
+      console.log(`Error: ${err.message}`)
+      res.status(400).json({
+        message: `Unable to get packages by name!`,
+        error: err.message,
+      })
+    })
+}
+
+//Update
+/*
+ * just store everything you want to update in the body. 
+ * Send every other field as null or just don't send it maybe?
+ ! Just make sure you pass the id of the package you want to update!
+ */
+const package_update = async (req, res) => {
+  const body = _.omitBy(req.body, _.isNil)
+  await Package.findOneAndUpdate(body._id, body, (err, result) => {
+    if (err) {
+      console.warn(
+        `An error occured in package_update @ time: ${helpers.getTimeStamp()}`
+      )
+      console.log(`Error: ${err.message}`)
+      res.status(400).json({
+        message: 'Unable to update package!',
+      })
+    } else {
+      console.log(`Package updated @ time: ${helpers.getTimeStamp()}`)
+      res.status(200).json({
+        message: 'Package updated!',
+        id: result._id,
+      })
+    }
+  })
+}
+
+//send the id of the package in the query params(call it id), the status of publish will then be toggled
+const package_publish_or_unpublish = async (req, res) => {
+  try {
+    let package = await Package.findbyid(req.query.id).catch((err) => {
+      console.warn(
+        `An error occured in package_publish_or_unpublish @ time: ${helpers.getTimeStamp()}`
+      )
+      console.log(`Error: ${err.message}`)
+    })
+    package.published = !package.published
+    package.save()
+    res.status(20).json({
+      message: `package has been updated to: ${package.publiched}`,
+      id: package._id,
+    })
+  } catch (err) {
+    console.warn(
+      `An error occured in package_publish_or_unpublish @ time: ${helpers.getTimeStamp()}`
+    )
+    console.log(`Error: ${err.message}`)
+    res.status(400).json({
+      message: 'Unable to updated package!',
+      error: err.message,
+    })
+  }
 }

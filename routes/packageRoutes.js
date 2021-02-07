c8d310470e89e1168308b5397b6c4627028182a5
Brad Walsh <bradley.walsh.97@gmail.com>
2021-02-06 23:25:24 -0500

    fixed typo with some routes

--- routes/packageRoutes.js
+++ routes/packageRoutes.js
@@ -6,14 +6,14 @@ const router = express.Router()
 router.post('/', packageController.package_create)
 //Retrieve
 router.get('/', packageController.package_get_all)
-router.get('/publish', packageController.package_get_published)
-router.get('/unpublished', packageController.package_get_unpublished)
+router.get('/publish', packageController.package_get_all_published)
+router.get('/unpublished', packageController.package_get_all_unpublished)
 router.get('/garage', packageController.package_get_by_garage)
 router.get('/name', packageController.package_get_by_name)
 //Update
-router.put('/', appointmentController.package_update)
-router.put('/publish', appointmentController.package_publish_or_unpublish)
+router.put('/', packageController.package_update)
+router.put('/publish', packageController.package_publish_or_unpublish)
 //Delete
-router.delete('/', appointmentController.package_delete)
+router.delete('/', packageController.package_delete)
 
 module.exports = router

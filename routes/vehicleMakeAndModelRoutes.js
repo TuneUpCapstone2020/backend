f5beafde6c32e9ef1c68cfdaae71c76e44d5c056
Brad Walsh <bradley.walsh.97@gmail.com>
2021-02-16 14:26:32 -0500

    Now exporting routes

--- routes/vehicleMakeAndModelRoutes.js
+++ routes/vehicleMakeAndModelRoutes.js
@@ -5,10 +5,12 @@ const router = express.Router()
 //create - none required
 //Retrieve
 router.get('/make', vehicleMakeAndModelController.vehicle_make_get_all)
-router.get('/all', vehicleMakeAndModelController.vehicle_models_get_all)
+router.get('/', vehicleMakeAndModelController.vehicle_models_get_all)
 router.get(
   '/model',
   vehicleMakeAndModelController.vehicle_models_get_by_make_id
 )
 //Update - none required
 //Delete - none required
+
+module.exports = router

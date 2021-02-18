3b69f8f400ea0b0c73e61a2471dbe09d512596d4
Brad Walsh <bradley.walsh.97@gmail.com>
2021-02-18 01:20:23 -0500

    Added new field

--- models/appointment.js
+++ models/appointment.js
@@ -5,87 +5,98 @@ const CatalogProduct = require('./catalogProduct')
 //const Vehicle = require('./vehicle')
 const Client = require('./client')
 
-const appointmentSchema = new Schema({
+const appointmentSchema = new Schema(
+  {
     garageId: {
-        type: Schema.Types.ObjectId,
-        required: true
+      type: Schema.Types.ObjectId,
+      required: true,
     },
     date: {
-        type: Date,
-        required: true,
-        index: true
+      type: Date,
+      required: true,
+      index: true,
     },
     start_time: {
-        type: Date
+      type: Date,
     },
     end_time: {
-        type: Date
+      type: Date,
     },
     skill_level: {
-        type: Number,
-        required: true,
-        default: 1
+      type: Number,
+      required: true,
+      default: 1,
     },
     /*finalBill: {
         type: Boolean,
         required: true
     },*/
-    final_price: { //to be generated when appointment is complete
-        type: Number //stored in cents
+    final_price: {
+      //to be generated when appointment is complete
+      type: Number, //stored in cents
     },
     employee_num: {
-        type: Number,
-        index: true
+      type: Number,
+      index: true,
     },
-    services: [{
+    services: [
+      {
         service: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'CatalogService', 
-            default: undefined
-        },
-        quantity: { //ie tire change x4 
-            type: Number,
-            default: 1
-        }
-    }],
-    total_estimated_time: {
-        type: Number //stored in minutes,
-    },
-    products: [{
-        product: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'CatalogProduct',
-            default: undefined
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'CatalogService',
+          default: undefined,
         },
         quantity: {
-            type: Number,
-            default: 1
-        }
-    }],
-    labour_time: { //!check for this before marking as complete
-        type: Number, //stored in seconds
-        default: 0
+          //ie tire change x4
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+    total_estimated_time: {
+      type: Number, //stored in minutes,
+    },
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'CatalogProduct',
+          default: undefined,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+    labour_time: {
+      //!check for this before marking as complete
+      type: Number, //stored in seconds
+      default: 0,
     },
     client: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Client',
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Client',
+      required: true,
     },
     discount: {
-        type: Number, //format: 10=10% off, 15=15% off, etc
-        default: 0
+      type: Number, //format: 10=10% off, 15=15% off, etc
+      default: 0,
+    },
+    description: {
+      type: String,
     },
     archived: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     deleted: {
-        type: Boolean,
-        default: false
-    }
-
-
-}, { timestamps: true })
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+)
 
 const Appointment = mongoose.model('Appointment', appointmentSchema)
 module.exports = Appointment

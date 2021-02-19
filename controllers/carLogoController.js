e1f5dc0418ab7288019010307bad181ddef7a47b
Brad Walsh <bradley.walsh.97@gmail.com>
2021-02-19 15:31:06 -0500

    minor changes

--- controllers/carLogoController.js
+++ controllers/carLogoController.js
@@ -1,5 +1,9 @@
 const Logo = require('../models/carLogoSchema')
+let multer = require('multer')
 
 //todo: error handler
 
+//Create image
+const create_all_default_images = () => {}
+//in query params: makeId
 const logo_get_by_make_id = (req, res) => {}

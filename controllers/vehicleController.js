ba95e4a67ae2afc5d999cad7274d8f2f5041042c
Brad Walsh <bradley.walsh.97@gmail.com>
2021-02-20 01:53:13 -0500

    improved error responses

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -182,13 +182,15 @@ const vehicle_delete = async (req, res) => {
     await Vehicle.findOneAndUpdate(
       { _id: req.query._id },
       { deleted: true },
+      { new: true },
       (err, result) => {
         if (err) {
           console.warn(`An error occured in vehicle_delete!`)
-          res.status(400).json({
-            message: 'An error occured!',
-            error: err.message,
-          })
+          console.log(`Error: ${err.message}`)
+          // res.status(400).json({
+          //   message: 'An error occured!',
+          //   error: err.message,
+          // })
         } else {
           console.log(`Vehicle deleted ${result._id}`)
           res.status(200).json({
@@ -199,7 +201,8 @@ const vehicle_delete = async (req, res) => {
       }
     )
   } catch (err) {
-    console.log(`An error occured in vehicle_delete!`)
+    console.warn(`An error occured in vehicle_delete!`)
+    console.log(`Error: ${err.message}`)
     res.status(400).json({
       message: 'An error occured!',
       error: err.message,

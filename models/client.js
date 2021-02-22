51ce879be4118608ff47921b250f16ed1acd82ec
Brad Walsh <bradley.walsh.97@gmail.com>
2021-02-21 21:13:03 -0500

    commented unnecessary logs

--- models/client.js
+++ models/client.js
@@ -118,8 +118,8 @@ clientSchema.statics.addVehicle = async function (clientId, vehicle) {
   const client = await this.findById(clientId).exec()
   if (client) {
     client.vehicles.push(vehicle._id)
-    console.log('Added new vehicle to client: ', vehicle)
-    console.log('Client profile is now ', client)
+    // console.log('Added new vehicle to client: ', vehicle)
+    // console.log('Client profile is now ', client)
     client.save()
   } else throw Error('Client not found')
 }

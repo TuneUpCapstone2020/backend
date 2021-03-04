1c63f271a6882b28bc2663f77ad64cb0f9ebe94e
Brad Walsh <bradley.walsh.97@gmail.com>
2021-03-04 02:55:31 -0500

    added route for new endpoint

--- routes/valetRouteRoutes.js
+++ routes/valetRouteRoutes.js
@@ -9,6 +9,7 @@ router.post('/', valetRouteController.route_create)
 //Retrieve
 router.get('/', valetRouteController.route_get_coordinates_by_route_id)
 router.get('/appointment', valetRouteController.route_get_coordinates_by_appointment_id)
+router.get('/address', valetRouteController.route_get_lat_and_long_from_address)
 
 //Update
 router.put('/', valetRouteController.route_add_point_by_appointment_id)

517f7d8fb30231bde8efcaa01e9bfe389dbd0176
Brad Walsh <bradley.walsh.97@gmail.com>
2021-03-05 22:29:09 -0500

    added image_attribute_key field

--- models/vehicle.js
+++ models/vehicle.js
@@ -71,6 +71,9 @@ const vehicleSchema = new Schema(
         attribute_image_url: {
           type: String,
         },
+        attribute_image_key: {
+          type: String,
+        },
       },
     ],
     health_attributes_summary: {

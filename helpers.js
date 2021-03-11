d4bc3228a84ffe6c4c79e7319a34bb3d08116973
Brad Walsh <bradley.walsh.97@gmail.com>
2021-03-10 21:46:20 -0500

    added new helper to create push notifications

--- helpers.js
+++ helpers.js
@@ -3,19 +3,11 @@ const jwt = require('jsonwebtoken')
 const VehicleMake = require('./models/vehicleMake')
 const VehicleModel = require('./models/vehicleModel')
 const Vehicle = require('./models/vehicle')
+const { keys } = require('lodash')
 require('dotenv').config()
 
 const getTimeStamp = () => {
   let date_ob = new Date()
-  // let date = ('0' + date_ob.getDate).slice(-2)
-  // let month = ("0" + (date_ob.getMonth() + 1)).slice(-2)
-  // let year = date_ob.getFullYear()
-  // let hours = date_ob.getHours()
-  // let minutes = date_ob.getMinutes()
-  // let seconds = date_ob.getSeconds()
-
-  // return `${seconds}:${minutes}:${hours} ${date}-${month}-${year}`
-  //return dateFormat(date_ob, 'hh:MM:ss dd-mm-yyyy')
   return dateFormat(date_ob, 'isoDateTime')
 }
 
@@ -52,9 +44,86 @@ const populateVehicleAttributes = async (vehicleId) => {
   console.log(`Done populating vehicle attributes @ time: ${getTimeStamp()}`)
 }
 
+/*
+ * Arguments:
+ *  clientId: id of client whom notif is to be sent to
+ *  text: the title of the notification
+ *  body: the body of the notification
+ */
+const createPushNotification = async (clientId, title, body) => {
+  client = Client.findById(clientId)
+  if (client.devicePlatform === 'android') {
+    let message = new gcm.message({
+      notification: {
+        title: title,
+        body: body,
+        notification: 'notification_important',
+      },
+    })
+
+    sender.send(
+      message,
+      {
+        registrationTokens: client.deviceId,
+      },
+      (err, response) => {
+        if (err) {
+          console.warn(
+            `An error occurred in send_push_notification @ time: ${helpers.getTimeStamp()}`
+          )
+          console.log(`Error: ${err.message}`)
+          // res.status(400).json({
+          //   message:
+          //     'Unable to send notification to client! Please contact them directly',
+          //   error: err.message,
+          // })
+          return {
+            message:
+              'Unable to send notification to client! Please contact them directly',
+            error: err.message,
+          }
+        } else {
+          console.log(
+            `Push notification sent to: 
+            \n client: ${client._id} 
+            \n device: ${client._id} 
+            \n time: ${helpers.getTimeStamp()}
+            \n response: ${response}`
+          )
+          return {
+            message: 'Push notification sent to client!',
+            client: client._id,
+            device: client.deviceId,
+            response: response,
+          }
+          // res.status(200).json({
+          //   message: 'Push notification sent to client!',
+          //   client: client._id,
+          //   device: client.deviceId,
+          //   response: response,
+          // })
+        }
+      }
+    )
+  } else if (client.devicePlatform === 'ios') {
+    console.warn(
+      `Somehow the client has an iPhone even though the app doesn't exist on ios???'`
+    )
+    return {
+      message: 'An error occurred',
+      error: 'Client has iPhone without iPhone app existing',
+    }
+    // res.status(400).json({
+    //   message: 'An error occurred',
+    //   error: 'Client has iPhone without iPhone app existing',
+    // })
+  }
+}
+
 module.exports = {
   getTimeStamp,
   getDecodedToken,
   populateVehicles,
   populateVehicleAttributes,
+  createPushNotification,
 }

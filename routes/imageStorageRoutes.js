8c54140cae96e74c7540d0acf9b40a12a2d3d238
Brad Walsh <bradley.walsh.97@gmail.com>
2021-03-17 00:07:14 -0400

    changed route of old endpoint to give it to new one

--- routes/imageStorageRoutes.js
+++ routes/imageStorageRoutes.js
@@ -5,6 +5,7 @@ const { modelName } = require('../models/catalogService')
 
 //Create
 router.post('/', imageStorageController.image_upload)
+router.post('/inspection', imageStorageController.image_upload_inspection_image)
 router.post('/logo', imageStorageController.image_upload_make_logo)
 //Retrieve
 router.get('/', imageStorageController.image_download)

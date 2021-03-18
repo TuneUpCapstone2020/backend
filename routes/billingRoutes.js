8f0f0af52ccd2f2bf515dfd062f09f5a356e8b32
Brad Walsh <bradley.walsh.97@gmail.com>
2021-03-18 01:52:50 -0400

    added route for new endpoint

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 //Retrieve
 router.get('/', billingController.generate_all_bills_for_client)
+router.get('/bill_detail', billingController.generate_appointment_cost_breakdown)
 
 
 module.exports = router
